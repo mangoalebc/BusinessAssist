1e119755b5d636b8a43cca9313a435e0f91c7c17
mangoalebc <bokangmangoale@gmail.com>
2017-02-28 14:33:01 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,73 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title></title>
+    </head>
+    <body>
+
+        <div id="container">
+		<div id="header">
+        	<h1>Business<span class="off">Assist</span></h1>
+            <h2>Welcome to Business Assist</h2>
+        </div>
+
+        <div id="menu">
+        	<ul>
+            	<li class="menuitem"><a href="index.php">Home</a></li>
+                <li class="menuitem"><a href="about.php">About</a></li>
+                <li class="menuitem"><a href="products.php">Products</a></li>
+                <li class="menuitem"><a href="services.php">Services</a></li>
+              <li class="menuitem"><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+
+        <div id="leftmenu">
+
+        <div id="leftmenu_top"></div>
+
+    <div id="leftmenu_main">
+
+        <h3>Links</h3>
+
+        <ul>
+            <li><a href="ElectronicsComputers.php">Electronics & Computers</a></li>
+            <li><a href="fashionBeauty.php">Fashion & Beauty</a></li>
+            <li><a href="hobbies.php">Hobbies & Interests</a></li>
+            <li><a href="homegarden.php">Home Garden & Tools</a></li>
+            <li><a href="officeBusiness.php">Office & Business</a></li>
+            <li><a href="kidsBabies.php">Kids & Baby</a></li>
+            <li><a href="sportsOutdoors.php">Sports & Outdoors</a></li>
+            <li><a href="foodDrinks.php">Food & Drinks</a></li>
+            <li><a href="vehicles.php">Vehicles</a></li>
+        </ul>
+    </div>
+
+
+        <div id="leftmenu_bottom"></div>
+        </div>
+
+	<div id="content">
+        <div id="content_top"></div>
+        <div id="content_main">
+            <h1>About us</h1><br/><br/>
+            <i class="fa fa-info-circle"></i>
+            <p>More information needed</p>
+
+        </div>
+        <div id="content_bottom"></div>
+
+            <div id="footer"><h3><a href="http://www.google.com"> BMangoale </a></h3></div>
+      </div>
+   </div>
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,87 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <title></title>
+    </head>
+    <body>
+
+        <div id="container">
+		<div id="header">
+        	<h1>Business<span class="off">Assist</span></h1>
+            <h2>Welcome to Business Assist</h2>
+        </div>
+
+        <div id="menu">
+        	<ul>
+            	<li class="menuitem"><a href="index.php">Home</a></li>
+                <li class="menuitem"><a href="about.php">About</a></li>
+                <li class="menuitem"><a href="products.php">Products</a></li>
+                <li class="menuitem"><a href="services.php">Services</a></li>
+              <li class="menuitem"><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+
+        <div id="leftmenu">
+
+        <div id="leftmenu_top"></div>
+
+    <div id="leftmenu_main">
+
+        <h3>Links</h3>
+
+        <ul>
+            <li><a href="ElectronicsComputers.php">Electronics & Computers</a></li>
+            <li><a href="fashionBeauty.php">Fashion & Beauty</a></li>
+            <li><a href="hobbies.php">Hobbies & Interests</a></li>
+            <li><a href="homegarden.php">Home Garden & Tools</a></li>
+            <li><a href="officeBusiness.php">Office & Business</a></li>
+            <li><a href="kidsBabies.php">Kids & Baby</a></li>
+            <li><a href="sportsOutdoors.php">Sports & Outdoors</a></li>
+            <li><a href="foodDrinks.php">Food & Drinks</a></li>
+            <li><a href="vehicles.php">Vehicles</a></li>
+        </ul>
+    </div>
+
+
+        <div id="leftmenu_bottom"></div>
+        </div>
+
+	<div id="content">
+        <div id="content_top"></div>
+        <div id="content_main">
+            <h1>Latest Products </h1><br/><br/>
+            <i class="fa fa-info-circle"></i>
+            
+
+            <a href="homegarden.php"><img src="images/lawnmower.png" width="150" height="150" alt="audi"/></a>
+            <a href="hobbies.php"><img src="images/book.png" width="100" height="150" alt="audi"/></a>
+            <a href="kidsBabies.php"><img src="images/stroller.png" width="150" height="150" alt="audi"/></a>
+            <a href="vehicles.php"><img src="images/audi.jpg" width="150" height="150" alt="audi"/></a><br/>
+
+           &nbsp;&nbsp;&nbsp; &nbsp;<a href="homegarden.php">Lawnmower</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="hobbies.php">Rogue Lawyer</a>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="kidsBabies.php">Baby Stroller</a>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="vehicles.php">Audi A4 2016</a>
+
+            <a href="sportsOutdoors.php"><img src="images/campsite-png-tent-transparent-21.png" width="150" height="150" alt="audi"/></a>
+            <a href="foodDrinks.php"><img src="images/nav_krush_381x273.png" width="150" height="100" alt="audi"/></a><br/>
+            &nbsp;<a href="homegarden.php">campsite tent transparent</a>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="homegarden.php">nav krush juice</a>
+        </div>
+        <div id="content_bottom"></div>
+
+            <div id="footer"><h3><a href="http://www.google.com"> BMangoale </a></h3></div>
+      </div>
+   </div>
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
